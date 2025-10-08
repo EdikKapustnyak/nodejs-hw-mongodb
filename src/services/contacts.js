d1eca59973523581a73682f1b6ae8b53b0fde7ad
kapustnyak <zned003@gmail.com>
2025-10-08 23:57:27 +0200

    1

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,35 +2,43 @@ import { ContactsCollection } from "../db/model/contacts.js";
 import { calculatePaginationData } from "../utils/calculatePaginationData.js";
 import { SORT_ORDER } from "../index.js";
 
-export const getAllContact = async (page, perPage, sortBy='_id', sortOrder=SORT_ORDER.ASC, filter={}) => {
+export const getAllContact = async (
+    page,
+    perPage,
+    sortBy = '_id',
+    sortOrder = SORT_ORDER.ASC,
+    filter = {}
+  ) => {
     const limit = perPage;
-    const  skip = (page - 1) * perPage;
-
-    const contactsQuery =  await ContactsCollection.find();
-
+    const skip = (page - 1) * perPage;
+  
+    const contactsQuery = ContactsCollection.find();
+  
     if (filter.name) {
-        contactsQuery.where('name').regex(new RegExp(filter.name, 'i'));
-      }
+      contactsQuery.where('name').regex(new RegExp(filter.name, 'i'));
+    }
     if (typeof filter.isFavourite === 'boolean') {
-        contactsQuery.where('isFavourite').equals(filter.isFavourite);
+      contactsQuery.where('isFavourite').equals(filter.isFavourite);
     }
     if (filter.contactType) {
-        contactsQuery.where('contactType').equals(filter.contactType);
+      contactsQuery.where('contactType').equals(filter.contactType);
     }
-
+  
+    const filterObj = contactsQuery.getQuery();
+  
     const [contactsCount, contacts] = await Promise.all([
-        contactsQuery.clone().countDocuments(),
-        contactsQuery.skip(skip).limit(limit).sort({ [sortBy]: sortOrder })
-      ]);
-      
-
+      ContactsCollection.countDocuments(filterObj), 
+      contactsQuery.skip(skip).limit(limit).sort({ [sortBy]: sortOrder }).exec() 
+    ]);
+  
     const paginationData = calculatePaginationData(contactsCount, page, perPage);
-
+  
     return {
-        data: contacts,
-        ...paginationData,
+      data: contacts,
+      ...paginationData,
     };
-};
+  };
+  
 
 export const getContactById = async (contactId) => { 
     const contact = await ContactsCollection.findById(contactId);
